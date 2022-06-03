bc9f4d1cf2042347c101c9cce5b68ac1722f8f69
JWCHOI <wkdudhksl@naver.com>
2022-06-03 20:47:36 +0900

    add - comment model

--- food/models.py
+++ food/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from user.models import UserModel
 
-# Create your models here.
+
+class Comment(models.Model):
+    class Meta:
+        db_table = "comments"
+
+    username = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    # store = models.ForeignKey(FoodModel, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=256)
+    star = models.FloatField()
