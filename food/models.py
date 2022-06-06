99c82cb400ebc1e07a205befde1c884038cf4c80
KIM HAJIN <rlagkwls629@gmail.com>
2022-06-06 22:47:44 +0900

    DB테스트완료

--- food/models.py
+++ food/models.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from django.db import models
 from user.models import UserModel
 
@@ -10,3 +11,20 @@ class Comment(models.Model):
     # store = models.ForeignKey(FoodModel, on_delete=models.CASCADE)
     comment = models.CharField(max_length=256)
     star = models.FloatField()
+
+class Food(models.Model):
+    class Meta:
+        db_table = "food"
+
+    store =  models.CharField(max_length=256)
+    img =  models.CharField(max_length=256)
+    address =  models.CharField(max_length=256)
+    tel =  models.CharField(max_length=256)
+    price =  models.CharField(max_length=256)
+    parking =  models.CharField(max_length=256)
+    close =  models.CharField(max_length=256)
+    holliday =  models.CharField(max_length=256)
+    staravg =  models.IntegerField(max_length=256)
+
+
+
--- food/views.py
+++ food/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-
+from . models import Food
 # Create your views here.
 
 def main_view(request):
     return render(request, 'food/main.html')
 
 def detail_view(request):
-    return render(request, 'food/detail.html')
+    if request.method == 'GET':
+        
+        all = Food.objects.get(id=1)
+        address = (all.address)
+        store = (all.store)
+        price = (all.price)
+        img = (all.img)
+        tel = (all.tel)
+        parking =  (all.parking)
+        close = (all.close)
+        holliday = (all.holliday)
+
+        return render(request, 'food/detail.html',{'address':address,'store':store,'img':img,'tel':tel,'parking':parking,'close':close,'holliday':holliday,'price':price})
 
