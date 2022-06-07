912dbcfdae3c372b5f9dc9f4e6483096f1d3ce84
JWCHOI <wkdudhksl@naver.com>
2022-06-07 16:09:36 +0900

    fix - detail_view

--- food/views.py
+++ food/views.py
@@ -7,29 +7,42 @@ from django.db.models import Avg
 def main_view(request):
     return render(request, 'food/main.html')
 
-
 @login_required
 def detail_view(request, id):
-    find_food = Food.objects.get(id=id)
-    food_id = find_food.id
-    food_store = find_food.store
-    food_staravg = round(find_food.staravg, 1)
+    all = Food.objects.get(id=id)
+    food_id = all.id
+    address = all.address
+    store = all.store
+    price = all.price
+    img = all.img
+    tel = all.tel
+    parking = all.parking
+    close = all.close
+    holiday = all.holiday
 
-    comments = Comment.objects.filter(store=food_store)
+    find_food = Food.objects.get(id=id)
+    if find_food.staravg != None:
+        food_staravg = round(find_food.staravg, 1)
+    else:
+        food_staravg = '없어요'
+
+    comments = Comment.objects.filter(store=store)
 
     if request.method == 'GET':
-      
-        all = Food.objects.get(id=1)
-        address = (all.address)
-        store = (all.store)
-        price = (all.price)
-        img = (all.img)
-        tel = (all.tel)
-        parking =  (all.parking)
-        close = (all.close)
-        holiday = (all.holiday)
         
-        return render(request, 'food/detail.html', {'id': food_id, 'staravg': food_staravg, 'comments': comments, 'address':address,'store':store,'img':img,'tel':tel,'parking':parking,'close':close,'holiday':holiday,'price':price})
+        return render(request, 'food/detail.html', {
+            'id': food_id, 
+            'staravg': food_staravg, 
+            'comments': comments, 
+            'address':address, 
+            'store':store, 
+            'img': img, 
+            'tel': tel, 
+            'parking':parking, 
+            'close': close, 
+            'holiday': holiday, 
+            'price': price
+            })
 
     elif request.method == 'POST':
         username = request.user
@@ -37,13 +50,39 @@ def detail_view(request, id):
         star = request.POST.get('star')
 
         if comment == '':
-            return render(request, 'food/detail.html', {'id': food_id, 'error': '코멘트 내용 없음', 'comments': comments})
+            return render(request, 'food/detail.html', {
+                'id': food_id, 
+                'error': '코멘트 내용 없음', 
+                'staravg': food_staravg, 
+                'comments': comments, 
+                'address': address, 
+                'store': store, 
+                'img': img, 
+                'tel': tel, 
+                'parking': parking, 
+                'close': close, 
+                'holiday': holiday, 
+                'price': price
+                })
         elif star == None:
-            return render(request, 'food/detail.html', {'id': food_id, 'error': '평점 입력하지 않음', 'comments': comments})
+            return render(request, 'food/detail.html', {
+                'id': food_id, 
+                'error': '평점 입력하지 않음', 
+                'staravg': food_staravg, 
+                'comments': comments, 
+                'address': address, 
+                'store': store, 
+                'img': img, 
+                'tel': tel, 
+                'parking': parking, 
+                'close': close, 
+                'holiday': holiday, 
+                'price': price
+                })
         else:
             model_comment = Comment()
             model_comment.username = username
-            model_comment.store = food_store
+            model_comment.store = store
             model_comment.comment = comment
             model_comment.star = star
             model_comment.save()
