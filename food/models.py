93fe9730df322901d44383b64d35b73c5696a66b
JWCHOI <wkdudhksl@naver.com>
2022-06-07 16:06:02 +0900

    add - Category model / fix - Food model

--- food/models.py
+++ food/models.py
@@ -2,22 +2,28 @@ from unicodedata import category
 from django.db import models
 from user.models import UserModel
 
+class Category(models.Model):
+    class Meta:
+        db_table = "categorys"
+    
+    category = models.CharField(max_length=256)
+    desc = models.TextField(max_length=256, blank=True)
 
 class Food(models.Model):
     class Meta:
-        db_table = "food"
+        db_table = "foods"
 
     store = models.CharField(max_length=256)
-    img = models.CharField(max_length=256)
-    address = models.CharField(max_length=256)
-    tel = models.CharField(max_length=256)
-    price = models.CharField(max_length=256)
-    parking = models.CharField(max_length=256)
-    close = models.CharField(max_length=256)
-    holiday = models.CharField(max_length=256)
-    staravg = models.FloatField()
-    # category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    category = models.CharField(max_length=256)
+    img = models.CharField(max_length=256, blank=True)
+    address = models.CharField(max_length=256, blank=True)
+    tel = models.CharField(max_length=256, blank=True)
+    price = models.CharField(max_length=256, blank=True)
+    parking = models.CharField(max_length=256, blank=True)
+    close = models.CharField(max_length=256, blank=True)
+    holiday = models.CharField(max_length=256, blank=True)
+    staravg = models.FloatField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # category = models.CharField(max_length=256)
 
 class Comment(models.Model):
     class Meta:
