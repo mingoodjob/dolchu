a5ad5e52352578fb01d887e0bb4e902331f232c9
minmincoding <psjlmk@gmail.com>
2022-06-10 18:52:32 +0900

    fix - 코드정렬, 불필요한 파일 삭제

--- static/detail.js
+++ static/detail.js
@@ -2,16 +2,16 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = {
         center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
         level: 2 // 지도의 확대 레벨
-    };  
+    };
 
 // 지도를 생성합니다    
-var map = new kakao.maps.Map(mapContainer, mapOption); 
+var map = new kakao.maps.Map(mapContainer, mapOption);
 
 // 주소-좌표 변환 객체를 생성합니다
 var geocoder = new kakao.maps.services.Geocoder();
 
 // 주소로 좌표를 검색합니다
-geocoder.addressSearch(' 충남 천안시 서북구 검은들3길 36 ', function(result, status) {
+geocoder.addressSearch(' 충남 천안시 서북구 검은들3길 36 ', function (result, status) {
 
     // 정상적으로 검색이 완료됐으면 
     if (status === kakao.maps.services.Status.OK) {
@@ -23,9 +23,9 @@ geocoder.addressSearch(' 충남 천안시 서북구 검은들3길 36 ', function
             map: map,
             position: coords
         });
-    
-        
+
+
         map.setCenter(coords);
 
-    } 
+    }
 });    
