3e8b635e65b15c33c48287d74af8414275e99cac
minmincoding <psjlmk@gmail.com>
2022-06-11 15:47:10 +0900

    fix - recommand

--- recommand/views.py
+++ recommand/views.py
@@ -17,25 +17,26 @@ def recommand(request):
     rating = pd.read_csv('user_rating.csv')
     store = pd.read_csv('store_info.csv')
 
-	# movieId를 기준으로 ratings 와 movies 를 결합함
+	# 상점명(store)을 기준으로 rating 와 store 를 결합함
     store_ratings = pd.merge(rating, store, on='store')
 
-	# user별로 영화에 부여한 rating 값을 볼 수 있도록 pivot table 사용
+	# user별로 상점에 부여한 rating 값을 볼 수 있도록 pivot table 사용
     title_user = store_ratings.pivot_table('rating', index='userid', columns='store')
 
 	# 평점을 부여안한 영화는 그냥 0이라고 부여
     title_user = title_user.fillna(0)
 
-	# 유저 1~610 번과 유저 1~610 번 간의 코사인 유사도를 구함
+	#  모든 유저간의 코사인 유사도를 구함
     user_based_collab = cosine_similarity(title_user, title_user)
 
 	# 위는 그냥 numpy 행렬이니까, 이를 데이터프레임으로 변환
     user_based_collab = pd.DataFrame(user_based_collab, index=title_user.index, columns=title_user.index)
 
-	# 1번 유저와 가장 비슷한 266번 유저를 뽑고,
+	# 현재 유저와 가장 비슷한 유저를 뽑고
     user = user_based_collab[userid].sort_values(ascending=False)[:10].index[1]
-	# 266번 유저가 좋아했던 영화를 평점 내림차순으로 출력
+	# 위에 유저의 가장높게 부여한 가게를 내림차순으로 정렬
     recommand_store = title_user.query(f"userid == {user}").sort_values(ascending=False, by=user, axis=1)
+    # 위에꺼 리스트로 10개만 가져옴
     recommand_list = recommand_store.columns.tolist()[:10]
     		
 	# pandas table to list
@@ -53,13 +54,13 @@ def recommand(request):
         store_list.append(store)
 
     star_result = []
-		# 1번 유저가 다크나이트를 보고 어떤 평점을 부여할지 예측
+	#위에 10개의 상점을 돌면서 현재 유저가 부여될 평점을 예측	
     for i in store_list:
         store = i.store
         weighted_sum = []
         weighted_user = []
         for i in range(1, 10):
-            # 해당 영화를 보고 평점을 부여한 사람들의 유사도와 평점만 추가 (즉, 0이 아닌 경우에만 계산에 활용)
+            # 해당 상점에 평점을 부여한 사람들의 유사도와 평점만 추가 (즉, 0이 아닌 경우에만 계산에 활용)
             if int(title_user[store][user_index_list[i]]) != 0:
                 # 평점 * 유사도 추가
                 weighted_sum.append(title_user[store][user_index_list[i]] * user_weight_list[i])
